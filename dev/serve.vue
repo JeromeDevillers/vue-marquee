735b85606f42a0c84b65f8834340eba622a66eef
Devillers Jerome <devillers.jerome@gmail.com>
2021-05-17 19:41:45 -0400

    First Push

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueMarquee from '@/vue-marquee.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueMarquee
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-marquee />
+  </div>
+</template>
